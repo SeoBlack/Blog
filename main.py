83a32aaf837b9ee8cbb71a447ef793dff683b8f8
SeoBlack <sooreennadum2001@gmail.com>
2023-01-26 19:41:00 +0200

    in main, changing from debug mode

--- main.py
+++ main.py
@@ -229,4 +229,4 @@ def delete_comment(comment_id,post_id):
     return redirect(url_for("show_post",post_id=post_id))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000)
